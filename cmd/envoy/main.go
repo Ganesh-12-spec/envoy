e99d4127a3ab16dc825d172561bef1881929bd2e
Ganesh Mhetre <mhetreg458@gmail.com>
2026-08-30 07:38:30 +0000

    feat: add init command

--- cmd/envoy/main.go
+++ cmd/envoy/main.go
@@ -12,8 +12,16 @@ var rootCmd = cobra.Command{
 	Use:   "envoy",
 	Short: "A secure environment for running applications",
 }
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize the environment",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Environment initialized")
+	},
+}
 
 func main() {
+	rootCmd.AddCommand(initCmd)
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error executing command:", err.Error())
