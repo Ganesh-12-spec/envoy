325d8bcd4bc53c67c7dba28226307e052335a631
Ganesh Mhetre <mhetreg458@gmail.com>
2026-08-31 06:30:55 +0000

    feat: add configuration loading

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,11 +9,25 @@ type Config struct {
 	CurrentEnvironment string `json:"current_environment"`
 }
 
-func Save(cfg Config, path string) error {
+func Save(cfg Config, path string)  error {
 	data, err := json.MarshalIndent(cfg, "", "  ")
 	if err != nil {
 		return err
 	}
 
 	return os.WriteFile(path, data, 0600)
+}
+func Load(path string) (Config, error) {
+	var cfg Config
+	data,err := os.ReadFile(path)
+	if err != nil {
+		
+		return cfg, err
+	}
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		
+		return cfg, err
+	}
+	return cfg, nil
 }
